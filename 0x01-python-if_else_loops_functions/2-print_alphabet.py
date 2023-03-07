cadefc61c6a18ba98fa4d13061ea1ae07d266905
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-07 15:30:06 -0800

    task2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-alphabet= range(97, 123)
-alphabet_list=list(alphabet)
+alphabet = range(97, 123)
+alphabet_list = list(alphabet)
 for characters in alphabet_list:
     a = chr(characters)
-    print("%s" %(a), end='')
+    print("%s" % (a), end='')
