9b4d5d1d518340eb5232ee043730065ddc69f755
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-07 07:47:59 -0800

    task 9

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,1 +1,2 @@
-Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex.
+#!/bin/python3
+print('Beautiful better than ugly,Explicit better than implicit,Simple is better')
