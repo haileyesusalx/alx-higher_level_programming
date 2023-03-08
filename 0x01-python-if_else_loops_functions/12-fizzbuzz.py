759fa6016d109a3b4b747c28ef9a89d3ba313e97
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-08 10:09:02 -0800

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    number = 5
-    Fizz = number % 3
-    Buzz = number % 5
-    if Fizz == 0:
-        print("Fizz")
-    elif Buzz == 0:
-        print("Buzz")
-    elif Fizz == 0 and Buzz == 0:
-        print("FizzBuzz")
-    else:
-        print()
+    number = range(1, 101)
+    for i in number:
+        Fizz = i % 3
+        Buzz = i % 5
+        if Fizz == 0 and Buzz == 0:
+            print("FizzBuzz", end=' ')
+        elif Fizz == 0 and Buzz != 0:
+            print("Fizz", end=' ')
+        elif Fizz != 0 and Buzz == 0:
+            print("Buzz", end=' ')
+        elif Fizz != 0 and Buzz != 0:
+            print(i, end=' ')
