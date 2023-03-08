e3a043dec7fd30516ad8504014a4b8ffe3c3b3d7
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-08 10:54:00 -0800

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def islower(string):
-    if string = 
-
-result = islower("z")
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
