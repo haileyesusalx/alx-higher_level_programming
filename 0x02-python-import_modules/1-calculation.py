adcf331e5bd0d200f185ccae615d8c3d38548ff5
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-09 15:10:33 -0800

    task 0 and 1 update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 
-
-a = 10
-
-b = 5
-
-c = add(10, 5)
-d = sub(10, 5)
-e = mul(10, 5)
-f = div(10, 5)
-print("{} + {} = {}".format(a, b, c))
-print("{} - {} = {}".format(a, b, d))
-print("{} * {} = {}".format(a, b, e))
-print("{} / {} = {}".format(a, b, f))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
