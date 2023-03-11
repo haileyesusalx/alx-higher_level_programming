d767f636fad1900c21d468629ec91c4b221184d9
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-11 09:16:49 -0800

    task update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     value = my_list[idx]
-    return value
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        return value
