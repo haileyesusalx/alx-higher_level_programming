83a96bd7caae96f0573ed229b731eb927ef6a62b
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-11 12:39:47 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
             print("{}".format(element), end=' ')
-        print()
+        print("{}".format(''))
