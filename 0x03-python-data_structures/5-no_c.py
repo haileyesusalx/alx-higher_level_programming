359e33b496ca99dd813141bc736423ccb47569cb
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-11 12:34:55 -0800

    task 6

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string1 = []
+    for x in list(my_string):
+        if x != "c":
+            return my_string1.append(x)
+        elif x != "C":
+            return my_string1.append(x)
+        else:
+            return  
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,4 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a(x,y) = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+    return (x, y
