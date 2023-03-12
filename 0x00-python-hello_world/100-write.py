f9c8c62acc71e4e86147f16fa47889fba5040da8
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-12 12:09:05 -0700

    task 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,7 @@
-#!/bin/python3
+#!/usr/bin/python3
 import sys
 
 message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(message)
 
-# Exit with status code 1
 sys.exit(1)
