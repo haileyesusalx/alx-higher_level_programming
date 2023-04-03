abfa96927ed4b95252cf1c94ed7ad4927e76dab0
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-04-03 13:00:51 -0700

    task 4

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """ retrive value at new class crated """
+    """ retrieve value at new class created """
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
@@ -30,9 +30,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
@@ -42,7 +42,7 @@ class Rectangle:
 
     """ calculate perimeter of rectangle """
     def perimeter(self):
-        a = self.__width
-        b = self.__height
-        p = ((2 * a) + (2 * b))
-        return p
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2 * (self.__width + self.__height))
