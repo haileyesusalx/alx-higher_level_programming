fe071e011121386792d3b8f2ef7e1513adb6c547
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-04-03 09:20:29 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,15 +3,17 @@
 
 
 class Rectangle:
+    """ new class crated """
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
 
-    """ represent a rectangle """
+    """ represent a rectangle width """
     @property
     def width(self):
         return self.__width
 
+    """ represent a rectangle width for a value"""
     @width.setter
     def width(self, value):
         self.__width = value
@@ -20,10 +22,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("ValueError")
 
+        """ represent a rectangle height """
     @property
     def height(self):
         return self.__height
 
+    """ represent a rectangle height at a value """
     @height.setter
     def height(self, value):
         self.__height = value
@@ -31,6 +35,9 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("ValueError")
+
+
+""" represent formated print"""
 
 
 def __repr__(self):
