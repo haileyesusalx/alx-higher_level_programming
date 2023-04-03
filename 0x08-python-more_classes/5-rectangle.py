7be1b2a9a17dcdd4f366a18cfa4aa32018ce8772
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-04-03 14:11:57 -0700

    task 5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -3,10 +3,12 @@
 
 
 class Rectangle:
+    """new class"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
+    """width"""
     @property
     def width(self):
         return self.__width
@@ -20,6 +22,7 @@ class Rectangle:
         else:
             self.__width = value
 
+    """height"""
     @property
     def height(self):
         return self.__height
@@ -33,23 +36,28 @@ class Rectangle:
         else:
             self.__height = value
 
+    """area"""
     def area(self):
         return self.width * self.height
 
+    """perimeter"""
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
         else:
             return 2 * (self.width + self.height)
 
+    """str"""
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
         else:
             return "\n".join(["#" * self.width] * self.height)
 
+    """repr"""
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self.height)
 
+    """del"""
     def __del__(self):
         print("Bye rectangle...")
