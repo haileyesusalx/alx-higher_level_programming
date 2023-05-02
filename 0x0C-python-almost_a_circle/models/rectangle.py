28d7044d15abe2a2b4310e123d23bd06d83b9d5d
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-01 18:41:37 -0700

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Module for the Rectangle class"""
-
-from models.base import Base
+"""Rectangle module."""
 
 
-class Rectangle(Base):
-    """Rectangle class inheriting from Base"""
-
+class Rectangle:
+    """Rectangle class."""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Instantiation of a rectangle"""
-
-        super().__init__(id)
-
+        """Initialize a Rectangle instance."""
+        self.id = id
         self.width = width
         self.height = height
         self.x = x
@@ -19,77 +14,79 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Getter for width"""
+        """Getter method for width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for width"""
+        """Setter method for width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Getter for height"""
+        """Getter method for height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for height"""
+        """Setter method for height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
-        """Getter for x"""
+        """Getter method for x."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for x"""
+        """Setter method for x."""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
-        """Getter for y"""
+        """Getter method for y."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for y"""
+        """Setter method for y."""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+        self.__y = value
 
     def area(self):
-        """Returns the area of the rectangle"""
+        """Calculate and return the area of the Rectangle instance."""
         return self.__width * self.__height
 
     def display(self):
-        """Prints the rectangle using the '#' character"""
+        """Print the Rectangle instance with the '#' character."""
         for i in range(self.__y):
             print()
         for i in range(self.__height):
-            print(" " * self.__x, end="")
-            print("#" * self.__width)
+            print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
-        """Returns the string representation of the rectangle"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height))
+        """Return the string representation of the Rectangle instance."""
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
+
+    def update(self, *args):
+        """Assign an argument to each attribute."""
+        attrs = ["id", "width", "height", "x", "y"]
+        for index, arg in enumerate(args):
+            setattr(self, attrs[index], arg)
