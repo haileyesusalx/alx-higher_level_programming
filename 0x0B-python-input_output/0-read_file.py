3757b757a44043be23946e268899bd9d7c88a8f3
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-03 09:58:21 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+Module for reading a text file (UTF8) and printing its content to stdout.
+"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+    Reads a text file (UTF8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+
+    Returns:
+        None.
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
