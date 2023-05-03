7a15a2ccaeb40e81cd849e4665d331775d8dc473
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-03 11:57:41 -0700

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an Exception with the message
+        area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates value
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
