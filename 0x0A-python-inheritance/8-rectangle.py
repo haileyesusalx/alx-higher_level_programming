299bee7be19d4cff978a4852249e9f2a8cd2ecac
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-03 12:48:46 -0700

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -14,21 +14,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        """Returns string representation of the object."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        """Returns the area of the rectangle."""
-        return self.__width * self.__height
-
-    @property
-    def width(self):
-        """Returns the width of the rectangle."""
-        return self.__width
-
-    @property
-    def height(self):
-        """Returns the height of the rectangle."""
-        return self.__height
