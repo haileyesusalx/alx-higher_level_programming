59528edfada3f230c0d4225140ae7e06df65d57f
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-03 18:41:04 -0700

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define afile"""
+
+
+def append_write(filename="",text=""):
+    """ append """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
