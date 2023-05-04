82e543e1bcd3d2cb611336b67d646eb3fe1cbf2f
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-03 19:33:17 -0700

    task

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,7 +4,9 @@
 
 class Student:
     """define"""
+
     def __init__(self, first_name, last_name, age):
+        """define"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
