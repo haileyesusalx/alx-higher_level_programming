b5329f3d49aa1594f376caf350a307281fa56240
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-06-13 15:58:18 -0700

    task update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-const myVar = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'JavaScript is amazing';
-
+const myVar = [ 'C is fun', 'Python is cool', 'JavaScript is amazing' ]
 for (let i = 0; i < 3; i++) {
-  if (i === 0) {
-    console.log(myVar);
-  } else if (i === 1) {
-    console.log(myVar2);
-  } else {
-    console.log(myVar3);
-  }
+	console.log(myVar[i]);
 }
