307562d2df246b61064e56b1988da8e387a9c9f9
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-06-13 16:41:30 -0700

    task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,10 +4,6 @@ const arg = process.argv.slice(2);
 
 if (arg[0] === undefined) {
   console.log('No argument');
-} else if (arg[1] === undefined) {
-  console.log(arg[0]);
-} else if (arg[2] === undefined) {
-  console.log(arg[0], arg[1]);
 } else {
-  console.log(arg[0], arg[1], arg[2]);
+  console.log(arg[0]);
 }
