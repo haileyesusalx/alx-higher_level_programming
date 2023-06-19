e380177ee437cf3636f3cd25c707596804bc562f
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-06-18 18:14:18 -0700

    task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (uppercase N) from the database hbtn_0e_0_usa.
+Lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
@@ -8,7 +9,8 @@ import sys
 
 if __name__ == "__main__":
     """
-    Connects to a MySQL server and lists all states with a name starting with N from the specified database.
+    Connects to a MySQL server and lists all states with a name starting
+    with N from the specified database.
     """
     # Get command line arguments
     username = sys.argv[1]
@@ -16,12 +18,19 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
     # Create a cursor object to execute queries
     cursor = db.cursor()
 
-    # Execute the query to select states with name starting with N and order by id
+    " Execute the query to select states with name starting
+    with N and order by id "
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch all the rows and print them
