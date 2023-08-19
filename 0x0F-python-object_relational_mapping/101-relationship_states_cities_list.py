17cb5e171b8cee0290426cd9b58fc2f8879a3c4c
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-08-19 16:47:33 -0700

    tasks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding City objects
-contained in the database hbtn_0e_101_usa
+This script lists all State objects and corresponding City objects
+contained in the database hbtn_0e_101_usa.
 """
 
 import sys
-from relationship_state import State, Base
-from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
-
-def list_states_cities(username, password, database):
+if __name__ == "__main__":
     """
-    Lists all State objects and corresponding City objects from the database
+    Connects to a MySQL server, retrieves and prints the State and City data.
     """
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f'{state.id}: {state.name}')
-        for city in state.cities:
-            print(f'\t{city.id}: {city.name}')
-
-    session.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print('Usage: ./101-relationship_states_cities_list.py <username> <password> <database>')
-        sys.exit(1)
-
+    # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    list_states_cities(username, password, database)
+    # Connect to MySQL server
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{database}',
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and print State and City data
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
+
+    session.close()
