18de3228c69e5968845d0a63043e2b3675f1a625
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-08-19 15:32:49 -0700

    task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,15 @@ with N (upper N) from the database hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+def filter_states_by_name_starting_with_n(username, password, database):
+    """
+    Retrieves and prints all states with names starting with 'N' from the database.
 
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+    """
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database)
@@ -31,3 +34,15 @@ if __name__ == "__main__":
     # Close cursor and database connection
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    """
+    Main function to execute the script when run as the main program.
+    """
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the function to filter and print states
+    filter_states_by_name_starting_with_n(username, password, database)
