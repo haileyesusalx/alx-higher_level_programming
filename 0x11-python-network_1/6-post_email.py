8607209e323d118b464a85f3db2a41ff29408139
haileyesus kefyalew <117822035+haileyesusalx@users.noreply.github.com>
2023-09-01 23:58:48 +0300

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-"""
-This script sends a POST request to a given URL with an email parameter and displays the response body.
-
-Usage: ./6-post_email.py <URL> <email>
-
-Arguments:
-    URL (str): The URL to which the POST request will be sent.
-    email (str): The email address to be sent as a parameter.
-
-Example:
-    ./6-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
-"""
-
 import requests
 import sys
 
@@ -26,9 +13,15 @@ if __name__ == "__main__":
     # Create a dictionary with the email as a parameter
     data = {'email': email}
 
-    # Send a POST request to the URL with the email parameter
-    response = requests.post(url, data=data)
-
-    # Display the email and response body
-    print("Your email is:", email)
-    print(response.text)
+    try:
+        # Send a POST request to the URL with the email parameter
+        response = requests.post(url, data=data)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            print("Your email is:", email)
+            print(response.text)
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
