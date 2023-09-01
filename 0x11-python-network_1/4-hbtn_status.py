92592a76bdec3727486dddf2c131c73511498c36
haileyesus kefyalew <117822035+haileyesusalx@users.noreply.github.com>
2023-09-01 23:36:10 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""fetches https://intranet.hbtn.io/status
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header of the response.
+"""
 import requests
 
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("     - type:", type(r.text))
-    print("     - content:", r.text)
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
